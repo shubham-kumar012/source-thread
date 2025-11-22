c55b863f6cf3da70662f22e8fb59285acce7ccdc
shubham <shubhampal7083@gmail.com>
2025-11-22 10:51:03 +0530

    blog crousel updated

--- frontend/src/components/home/BlogCarousel.jsx
+++ frontend/src/components/home/BlogCarousel.jsx
@@ -15,12 +15,12 @@ const CategorySection = () => {
 
     const fadeSlideUp = {
         hidden: { opacity: 0, y: 40 },
-        visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeOut" } }
+        visible: { opacity: 1, y: 0, transition: { duration: 0.4, ease: "easeOut" } }
     };
 
     const fadeIn = {
         hidden: { opacity: 0 },
-        visible: { opacity: 1, transition: { duration: 0.8 } }
+        visible: { opacity: 1, transition: { duration: 0.4 } }
     };
 
     return (
