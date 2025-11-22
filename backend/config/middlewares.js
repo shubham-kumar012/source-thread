f60b74bb56ff9bb91613b1e743311e8252c7dc5a
shubham <shubhampal7083@gmail.com>
2025-11-22 10:52:13 +0530

    render database migrated into strapi

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,14 +1,21 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   {
-    name: "strapi::cors",
+    name: 'strapi::security',
     config: {
-      enabled: true,
-      origin: ["*"],   // or your frontend URL
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
     },
   },
+  "strapi::cors",
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
